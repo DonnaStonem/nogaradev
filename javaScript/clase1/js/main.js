02ba3a7408da434d99b87113736abf11fe10fbb4
Donna <dannalizbethmatiasm@gmail.com>
2022-03-08 23:06:38 -0600

    Clase 1 JavaScript

---
+++ javaScript/clase1/js/main.js
@@ -0,0 +1,62 @@
+/* alert('Hola'); */
+/* var para crear una variable que no se ha hecho con anterioridad */
+
+/* var edad = 5;
+var nombre;
+
+nombre = 9;
+
+var fondos = 100;
+
+fondos = 150;
+fondos = 120;
+
+
+var nombre = 'DonnaStonem';
+
+var whats = nombre;
+var face = nombre;
+var ig = nombre;
+var tw = nombre;
+
+console.log(nombre);
+console.log(whats);
+console.log(face);
+console.log(ig);
+console.log(tw); */
+
+/* var casado = true;
+var muerto = false;
+if (casado) {
+    console.log('Felicidades!');
+}
+ */
+/*Borra lo de la consola crtl + l */
+
+/* console.log(prompt('¿Tienes hambre?'));
+
+var name = prompt('Cual es tu nombre?');
+console.log(name); */
+
+
+
+/* var comer = true;
+var caminar = false;
+
+var hambre = confirm('Tienes hambre?');
+
+if (comer) {
+    console.log('Come algo :v');
+}
+
+else{
+    console.log('Vamos a caminar');
+}
+ */
+
+var num1 = 357;
+var num2 = 62.4;
+var num3 = -982;
+
+var suma = num1 + 3;
+console.log(suma);
