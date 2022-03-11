d806840d97cf779bc384b4c5edab45370f581b69
Donna <dannalizbethmatiasm@gmail.com>
2022-03-10 20:01:43 -0600

    clase2 js

---
+++ javaScript/clase2/js/main.js
@@ -0,0 +1,67 @@
+/* Operadores de comparación */
+/* 
+   == igualdad
+   === igual que estricto
+   !== diferente de estricto
+   != diferente de
+   > mayor que
+   < menor que
+   >= mayor o igual que
+   <= menor o igual que
+
+
+*/
+/* var edadCliente = 18;
+var minEdad = 18; */
+
+
+
+/* var edadCliente = 12;
+var maxEdad = 12; 
+
+if (edadCliente <= maxEdad) {
+    console.log('Puedes pasar');
+} */
+
+/* 
+var num1 = 0;
+var num2 = false;
+
+console.log(num1 == num2);
+
+var num1 = 1;
+var num2 = true;
+
+console.log(num1 == num2);
+
+var num1 = 1;
+var num2 = true;
+
+console.log(num1 === num2);
+
+console.log(num1 != num2); 
+
+if (edadCliente >= minEdad) {
+    console.log('Numero1 es mayor que numero2');
+}  */
+
+
+/* OPERADORES LÓGICOS 
+
+    &&
+    ||
+
+
+*/
+var mensaje = true;
+var visto = false;
+
+var leGusto = ' ';
+
+if (mensaje == true && visto == false) {
+    leGusto = true;
+    console.log('Le gusto 7u7');
+}
+else {
+    console.log('Echale ganas brou');
+}
