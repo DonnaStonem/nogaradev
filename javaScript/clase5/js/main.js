64cdc068dd4fb70df2cca27ff28eca751dbd8bf8
Donna <dannalizbethmatiasm@gmail.com>
2022-03-29 21:20:46 -0600

    clase5

---
+++ javaScript/clase5/js/main.js
@@ -0,0 +1,34 @@
+/*var lista = [1,8,4,7,3,6,5,2,0];
+
+ for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+} */
+/* var index = 0;
+
+while (index <= 50) {
+    console.log(index);
+    index = index + 2;
+}
+
+
+var indexx = 0;
+
+do {
+    console.log(indexx);
+    indexx = indexx + 5;
+} while (index <= 50);
+ */
+
+
+/* 
+undefined -> solo lo da la consola
+null -> un programador lo puede definir
+*/
+
+var usuario = null;
+console.log(usuario);
+
+
+var div1 = document.getElementsByTagName('div')[0];
+var div2 = document.getElementsByClassName('div')[0];
--- javaScript/ejercicios-2/js/main.js
+++ javaScript/ejercicios-2/js/main.js
@@ -15,14 +15,14 @@ multiplos de 5 por buzz */
 /* 
 for (var index = 1; index <= 100 ; index++)1{
     if (index % 3 == 0 && index % 5 == 0) {
-    console.log("fizz buzz");        
+    console.log(index + "fizz buzz");        
     } else if(index % 3 == 0) {
-        console.log("buzz");
+        console.log(index + "buzz");
     } else if(index % 5 == 0) {
-        console.log("buzz");
+        console.log(index + "buzz");
     }
     else {
-        console.log("buzz");
+        console.log(index + "buzz");
     }
 } */
 
