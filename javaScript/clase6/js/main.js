1eb343cb564d48e5c350222753cef0fedf0df3c3
Donna <dannalizbethmatiasm@gmail.com>
2022-04-22 00:31:53 -0500

    clase 6

---
+++ javaScript/clase6/js/main.js
@@ -0,0 +1,22 @@
+'use strict'
+
+
+var user = 'Donna';
+var btn = false;
+
+
+function hello(){
+    console.log('Bienvenida ' + user);
+}
+
+function cambiarColor(){
+    
+    document.getElementsByTagName('span')[1].style.backgroundColor = 'green';
+
+    if(btn){
+        div2.style
+    }
+}
+
+hello();
+
